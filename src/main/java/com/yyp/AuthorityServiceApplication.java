1e7f12b716011c7b1edfe79fa1b5ab0667efb2e9
youyongpeng <youyongpeng0613@qq.com>
2018-09-16 15:41:54 +0800

    first add

---
+++ src/main/java/com/yyp/AuthorityServiceApplication.java
@@ -0,0 +1,12 @@
+package com.yyp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorityServiceApplication.class, args);
+	}
+}
